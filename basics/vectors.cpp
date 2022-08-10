5f71f24a1c8c1d51d2f5fd86c57acb9553b2c684
ronmaa <ronmaa.7@gmail.com>
2022-08-10 17:40:51 +0530

    error due to declaring arr as array of vectors was rectified vector<string> arr(4) instead of vector<string> arr[4]

--- basics/vectors.cpp
+++ basics/vectors.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
 #include<vector>
+#include<string>
 using namespace std;
 int main(){
-	vector<int> index(4,0);
+	vector<int> index(4);
 
-	index.push_back(10);
-
+	vector<string> arr(4);
+	arr[0]="hello";
 	cout<<"The size of vector is "<<index.size()<<"\n";
 
 	cout<<"The capacity of vector is "<<index.capacity()<<"\n";
@@ -14,5 +15,9 @@ int main(){
 
 	for(int i=0;i<index.size();i++){
 		cout<<index[i]<<"\n";
+	}
+
+	for(string x:arr){
+		cout<<x<<endl;
 	}
 }
