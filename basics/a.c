b36d6b583e3fe4a42bffd2ab70982a0437da001f
ronmaa <ronmaa.7@gmail.com>
2022-08-11 17:07:14 +0530

    whenever we calculate a adress the values in stack are seen changing

---
+++ basics/a.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void sum(){
+	int a=10;
+	int b=20;
+}
+
+void hi(){
+	int m;
+	printf("%d",m);
+}
+
+int main(){
+	int b;
+	int c;
+	int d;
+	int e;
+	int f;
+	int g;
+	int h;
+	int i;
+	int j;
+	int k;
+	int a[1];
+	printf("%d",g);
+}
